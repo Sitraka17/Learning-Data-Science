6ebe43aa615a9ba772b4f192e678bb14ac67ea3c
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-01-26 23:34:14 +0100

    Update df.loc.py

--- cars/df.loc.py
+++ cars/df.loc.py
@@ -2,11 +2,12 @@
 import pandas as pd
 cars = pd.read_csv('cars.csv', index_col = 0)
 
-# Print out country column as Pandas Series
+# Print out country column as Pandas Series [  ]
 print(cars["country"])
 
-# Print out country column as Pandas DataFrame
+# Print out country column as Pandas DataFrame  [[   ]]
 print(cars[['country']])
 
 # Print out DataFrame with country and drives_right columns
 print(cars[[ "country", "drives_right"]])
+
