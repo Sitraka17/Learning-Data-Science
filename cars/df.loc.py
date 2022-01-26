4690421347bbb7290f3848c3dc29d04d8ec8d3ec
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-01-26 23:34:21 +0100

    Update df.loc.py

--- cars/df.loc.py
+++ cars/df.loc.py
@@ -11,3 +11,12 @@ print(cars[['country']])
 # Print out DataFrame with country and drives_right columns
 print(cars[[ "country", "drives_right"]])
 
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out first 3 observations
+print(cars[0:3])
+
+# Print out fourth, fifth and sixth observation
+print(cars[3:6])
