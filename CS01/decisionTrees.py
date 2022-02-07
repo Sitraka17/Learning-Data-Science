88d9781eaaa939ac50645692040d694d1bba3415
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-02-07 09:58:11 +0100

    Create decisionTrees.py

---
+++ CS01/decisionTrees.py
@@ -0,0 +1,7 @@
+#A decision tree is a sequence of yes/no questions that help the computer decide what to output.
+
+from sklearn import tree
+X = [[0, 0], [1, 1]]
+Y = [0, 1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, Y)
