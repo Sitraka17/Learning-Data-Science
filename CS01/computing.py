6f557c8025d5e7c109677d67f96745d4d406262c
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-02-07 09:07:31 +0100

    Create computing.py

---
+++ CS01/computing.py
@@ -0,0 +1,9 @@
+# Computing is the act of taking an input, processing it, and producing an output.
+2*2 
+# 
+def computing_function(x):
+  return 2 * x 
+
+print(computing_function(1)) 
+print(computing_function(2)) 
+print(computing_function(7)) 
