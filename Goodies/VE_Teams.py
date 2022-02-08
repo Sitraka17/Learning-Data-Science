1993757bbab95e43839777a1ab0cdce9beb1d0c5
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-02-08 16:17:00 +0100

    Create VE_Teams.py

---
+++ Goodies/VE_Teams.py
@@ -0,0 +1,13 @@
+import random
+
+def chunks(l, n):
+    """Faire des groupes"""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+n = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
+
+random.shuffle(n)
+
+list(chunks(n, 3))
+# [['B', 'H', 'G'], ['D', 'A', 'C'], ['E', 'F', 'I'], ['J', 'K']]
