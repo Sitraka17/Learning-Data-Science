b6f2e7cbb7e81d1b48ac79e11574bb3615c0ecad
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-02-08 17:10:22 +0100

    Update VE_Teams.py

--- Goodies/VE_Teams.py
+++ Goodies/VE_Teams.py
@@ -13,23 +13,3 @@ random.shuffle(n)
 list(chunks(n, 3))
 # [['B', 'H', 'G'], ['D', 'A', 'C'], ['E', 'F', 'I'], ['J', 'K']]
 
-
-
-
-
-
-
-
-
-#################################################################################################################
-#Method TWO: meh.
-people = [“Ian”, “Kevin”, “Carl”, “Veronica”, “Daniel”, “Christian”, “Sean”, “Frank”, “Phillip”, “Fiona”]
-number_people = len(people)
-number_of_teams = 3
-while number_people > 0 and number_of_teams > 0:
-team = random.sample(people, int(number_people/number_of_teams))
-for x in team:
-people.remove(x)
-number_people -= int(number_people/number_of_teams)
-number_of_teams -= 1
-print(team)
