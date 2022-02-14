6509aac5cf1095b969455b77ca446484d98019eb
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-02-14 11:48:33 +0100

    Update computing.py

--- CS01/computing.py
+++ CS01/computing.py
@@ -7,3 +7,8 @@ def computing_function(x):
 print(computing_function(1)) 
 print(computing_function(2)) 
 print(computing_function(7)) 
+
+
+#Programs aren't just there for computers to read and execute. To solve that last puzzle, 
+#you have to read and understand the program yourself. 
+#Programs written by humans generally exist to be read by both computers and humans.
