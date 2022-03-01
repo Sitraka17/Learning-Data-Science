a1b1165b4cfdf18ab7c6ce089e077656ddf523fa
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-03-01 10:00:48 +0100

    Update CS.py

--- CS01/CS.py
+++ CS01/CS.py
@@ -1,3 +1,10 @@
 Review and Reflect
 
 -- One algorithm can have many different implementations. 
+
+
+
+Searching for Solutions
+
+seven bridges and develop a systematic approach to solve this puzzle.
+Although it will seem impractical, this type of approach leverages the core strength of computers — quickly and tirelessly doing many tasks.
