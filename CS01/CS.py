7aa57885afc510849ec413dc6ad0b4850f48593b
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-03-18 23:59:50 +0100

    Update CS.py

--- CS01/CS.py
+++ CS01/CS.py
@@ -8,3 +8,12 @@ Searching for Solutions
 
 seven bridges to develop a *systematic* approach to solve this puzzle.
 Although it will seem impractical, this type of approach leverages the core strength of computers — quickly and tirelessly doing many tasks.
+
+
+Variables are letters or other symbols that stand in for changing or unknown values.
+
+They let us write down relationships between quantities in a simple, compact way.
+
+They allow us to represent unknown values.
+
+They can help reveal patterns in the information you start with, so that you can find a quick path between that information and the solution you're after.
