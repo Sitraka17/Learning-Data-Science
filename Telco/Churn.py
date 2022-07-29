63b3a4941ab69e61680caa475558344e09437221
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-07-29 23:39:46 +0200

    Update Churn.py

--- Telco/Churn.py
+++ Telco/Churn.py
@@ -1,3 +1,14 @@
+## Importing packages
+library(tidyverse) 
+library(MASS)
+library(car)
+library(e1071)
+library(caret)
+library(cowplot)
+library(caTools)
+library(pROC)
+library(ggcorrplot)
+
 
 telco['Churn'].value_counts()
 
