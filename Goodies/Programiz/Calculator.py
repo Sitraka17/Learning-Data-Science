d629160ae53c06fedec64b2b7342f768b2f18877
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-08-03 14:09:47 +0200

    Update Calculator.py

--- Goodies/Programiz/Calculator.py
+++ Goodies/Programiz/Calculator.py
@@ -3,7 +3,8 @@ num1 = input('Enter first number: ')
 num2 = input('Enter second number: ')
 
 # Add two numbers
-sum = float(num1) + float(num2)
+sum = float(num1) + float(num2) 
+#La méthode float() est utilisée pour renvoyer un nombre à virgule flottante à partir d’un nombre ou d’une chaîne.
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
