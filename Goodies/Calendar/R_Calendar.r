d8958b3ea4c3f875a539fe7f0fc9ec11bed0e9bb
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-08-09 12:08:35 +0200

    Update R_Calendar.r

--- Goodies/Calendar/R_Calendar.r
+++ Goodies/Calendar/R_Calendar.r
@@ -4,5 +4,14 @@ calendR()
 
 calendR(year = 2022) # Specify the year you prefer
 
+#################################################################
 calendR(year = 2022,
         start = "M") # Start the week on Monday
+
+#################################################################
+calendR(year = 2025,
+        start = "M",
+        special.days = c(9, 19, 56, 79, 102,  # Days to color !! 
+                         126, 257, 300, 342),
+        special.col = "lightblue",            # Color of the specified days
+        low.col = "white")                    # Background color of the rest of the days
