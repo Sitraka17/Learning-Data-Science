48f61ae148ffdb7b6c5e1adc6394b7391808b19b
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-08-26 09:32:41 +0200

    Update Nash_py.py

--- Economics/GameTheory/Nash_py.py
+++ Economics/GameTheory/Nash_py.py
@@ -1,3 +1,10 @@
 #Documentation: https://nashpy.readthedocs.io/en/stable/ 
 
 $ python -m pip install nashpy
+
+
+import nashpy as nash
+import numpy as np
+A = np.array([[0, -1, 1], [1, 0, -1], [-1, 1, 0]])
+rps = nash.Game(A)
+rps
