4dea9bf5efcd49d46dffaefbd40081607023cce5
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-08-26 09:12:21 +0200

    Update bmi.py

--- Py101/bmi.py
+++ Py101/bmi.py
@@ -1,3 +1,7 @@
+#Body mass index (BMI) is a value derived from the mass (weight) and height of a person. 
+#The BMI is defined as the body mass divided by the square of the body height, and is expressed in units of kg/m2, resulting from mass in kilograms and height in metres.
+
+
 # height_in and weight_lb are available as a regular lists
 
 # Import numpy
