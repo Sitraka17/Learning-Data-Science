ed70b203bc52c7116d24589134621fb90c0a67da
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-09-07 10:00:03 +0200

    Update PPTX.py

--- CS01/Tutorials/PPTX.py
+++ CS01/Tutorials/PPTX.py
@@ -3,3 +3,12 @@ $pip install python-pptx
 from pptx import Presentation
 
 prs=Presentation()
+
+
+lyt=prs.slide_layouts[0] # choosing a slide layout
+slide=prs.slides.add_slide(lyt) # adding a slide
+title=slide.shapes.title # assigning a title
+subtitle=slide.placeholders[1] # placeholder for subtitle
+title.text="Hey,This is a Slide! How exciting!" # title
+subtitle.text="Really?" # subtitle
+prs.save("slide1.pptx") # saving file
