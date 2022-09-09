0ed26f17befe3c6d2919658455dc018005eb75ef
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-09-09 11:21:38 +0200

    Create WS_Luxembourg.py

---
+++ Webscrapping/WS_Luxembourg.py
@@ -0,0 +1,8 @@
+soupe_obj = BeautifulSoup(html_obj, 'html.parser')
+
+for spans in soup.findAll('span', attrs={'id' : 'productTitle'}) :
+    nom_du_produit = spans.text.strip()
+    
+   $ conda install -c anaconda beautifulsoup4 
+   $ conda install -c anaconda lxml 
+   $ conda install -c anaconda requests 
