6bde87ef960e43bba18f9ac2ee9b640ae93f9673
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-09-09 11:22:04 +0200

    Update and rename Webscrapping/WS_Luxembourg.py to Webscrapping/Luxembourg/WS_Luxembourg.py

--- Webscrapping/Luxembourg/WS_Luxembourg.py
+++ Webscrapping/Luxembourg/WS_Luxembourg.py
@@ -6,3 +6,7 @@ for spans in soup.findAll('span', attrs={'id' : 'productTitle'}) :
    $ conda install -c anaconda beautifulsoup4 
    $ conda install -c anaconda lxml 
    $ conda install -c anaconda requests 
+
+
+from bs4 import BeautifulSoup as bs
+import requests
