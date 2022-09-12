c6f1767c230af205250f3740d340bed0744e8d26
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-09-12 22:47:07 +0200

    Update ROI_Stocks.py

--- Maths/UsefullFctions/ROI_Stocks.py
+++ Maths/UsefullFctions/ROI_Stocks.py
@@ -4,3 +4,7 @@ import matplotlib.pyplot as plt
 import pandas_datareader as web
 
 
+netflix = web.get_data_yahoo("NFLX",
+                            start = "2009-01-01",
+                            end = "2018-03-01")
+
