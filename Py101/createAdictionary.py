60692070d023d19c46f7dbd0fede243115ef915e
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-11-08 09:49:17 +0100

    Create createAdictionary.py

---
+++ Py101/createAdictionary.py
@@ -0,0 +1,12 @@
+#create a dictionary
+my_information = {'name': 'Dionysia', 'age': 28, 'location': 'Athens'}
+
+print(my_information)
+
+#check data type
+print(type(my_information))
+
+#output
+
+#{'name': 'Dionysia', 'age': 28, 'location': 'Athens'}
+#<class 'dict'>
