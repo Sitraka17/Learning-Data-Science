3e045ac5d43ae7c93d45ed7fba4963edac102a28
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-12-07 23:49:14 +0100

    Create Temperatures.py

---
+++ Py101/Temperatures.py
@@ -0,0 +1,11 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())  # the number of temperatures to analyse
+temps = input()  # the n temperatures expressed as integers ranging from -273 to 5526
+result =  0
+
+
