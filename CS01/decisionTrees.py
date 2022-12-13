07c3bf90d4f65c9987f914ee449a1f110d52fd6e
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-12-13 20:35:08 +0100

    Random Forest

--- CS01/decisionTrees.py
+++ CS01/decisionTrees.py
@@ -5,3 +5,8 @@ X = [[0, 0], [1, 1]]
 Y = [0, 1]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X, Y)
+
+
+###" The random forest is a classification algorithm consisting of many decisions trees. 
+#It uses bagging and feature randomness when building each individual tree to try to create an uncorrelated forest of trees 
+#whose prediction by committee is more accurate than that of any individual tree.
