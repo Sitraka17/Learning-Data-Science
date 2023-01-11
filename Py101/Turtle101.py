b617cdb28609f14a146308d2e0d61797219141b4
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-01-11 23:35:49 +0100

    Update Turtle101.py

--- Py101/Turtle101.py
+++ Py101/Turtle101.py
@@ -1,13 +1,15 @@
 from turtle import * # Loads commands used to draw
 
-color('red') 
-forward(200) # Moves the turtle 200 pixels right
+color('red') # Sets drawing color to red
+forward(200) # Moves the turtle 200 pixels forward, i.e. right
 left(90) # Turn left 90 degrees
 # Turtle is now facing up
-forward(200) 
-left(90) 
-forward(200)
-left(90) 
-forward(200)
+forward(200) # Moves 200 pixels forward, i.e. up 
+left(90) # Turn left 90 degrees
+# Turtle now facing left 
+forward(200) # Moves 200 pixels forward, i.e. left
+left(90) # Turn left 90 degrees
+# Turtle now facing down
+forward(200) # Complete bottom of the square
 
 bye() # Tells the turtle we're done writing commands
