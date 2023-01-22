26e1d2f27cf6be2d4b2c908bafcb027b117b58b6
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-01-22 19:15:10 +0100

    Update Python101.py

--- DataCamp/Python101.py
+++ DataCamp/Python101.py
@@ -1,3 +1,5 @@
+#    This should be used to unblock a situation after unsuccessfully searching. #
+
 # Is height_inches greater than 70 inches?
 print(height_inches > 70)
 
@@ -6,3 +8,16 @@ print(plate1 == "FRQ123")
 
 # Is fur_color not equal to "brown"?
 print(fur_color != "brown")
+
+
+# Select the dogs where Age is greater than 2
+greater_than_2 = mpr[mpr.Age > 2]
+print(greater_than_2)
+
+# Select the dogs whose Status is equal to Still Missing
+still_missing = mpr[Status == "Still Missing"]
+print(still_missing)
+
+# Select all dogs whose Dog Breed is not equal to Poodle
+not_poodle = mpr[mpr.Dog Breed != "Poddle"]
+print(not_poodle)
