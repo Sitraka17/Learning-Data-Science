9a2ffadd42ae4591f2923a331871b30ad95fe200
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-01-22 19:20:38 +0100

    Update Python101.py

--- DataCamp/Python101.py
+++ DataCamp/Python101.py
@@ -21,3 +21,18 @@ print(still_missing)
 # Select all dogs whose Dog Breed is not equal to Poodle
 not_poodle = mpr[mpr.Dog Breed != "Poddle"]
 print(not_poodle)
+
+
+
+# Select the dogs where Age is greater than 2
+greater_than_2 = mpr[mpr.Age > 2]
+print(greater_than_2)
+
+# Select the dogs whose Status is equal to Still Missing
+still_missing = mpr[mpr.Status == "Still Missing"]
+print(still_missing)
+
+# Select all dogs whose Dog Breed is not equal to Poodle
+not_poodle = mpr[mpr['Dog Breed'] != "Poodle"]        ####" HERE IT S INTERESTING 
+print(not_poodle)                                     ### Since the column name have fcking spaces and Capitals. You HAVE TO put it in [ ] and ' ' 
+#Yeah so please juste use _ 
