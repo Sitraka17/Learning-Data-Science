b2e11119e3f425659a524ad48d6bbf608158f06a
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-01-22 19:00:02 +0100

    Create Python101.py

---
+++ DataCamp/Python101.py
@@ -0,0 +1,8 @@
+# Is height_inches greater than 70 inches?
+print(height_inches > 70)
+
+# Is plate1 equal to "FRQ123"?
+print(plate1 == "FRQ123")
+
+# Is fur_color not equal to "brown"?
+print(fur_color != "brown")
