ca431a447646b5a8fe76fff53e83dc29a29064f7
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-01-25 21:53:34 +0100

    Update pltdotplot.py

--- DataCamp/pltdotplot.py
+++ DataCamp/pltdotplot.py
@@ -12,3 +12,28 @@ plt.legend()
 
 # Display plot
 plt.show()
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+fig = plt.figure()
+x = np.arange(10)
+y = 2.5 * np.sin(x / 20 * np.pi)
+yerr = np.linspace(0.05, 0.2, 10)
+
+plt.errorbar(x, y + 3, yerr=yerr, label='both limits (default)')
+
+plt.errorbar(x, y + 2, yerr=yerr, uplims=True, label='uplims=True')
+
+plt.errorbar(x, y + 1, yerr=yerr, uplims=True, lolims=True,
+             label='uplims=True, lolims=True')
+
+upperlimits = [True, False] * 5
+lowerlimits = [False, True] * 5
+plt.errorbar(x, y, yerr=yerr, uplims=upperlimits, lolims=lowerlimits,
+             label='subsets of uplims and lolims')
+
+plt.legend(loc='lower right')
