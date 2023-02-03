78efa318b17832074c07ba044b025a1b621f9add
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-02-03 22:17:35 +0100

    Update WebsiteDown.py

--- Webscrapping/WebsiteDown.py
+++ Webscrapping/WebsiteDown.py
@@ -1,3 +1,6 @@
 import urllib.request
 
 print(urllib.request.urlopen("https://www.stackoverflow.com").getcode())
+
+
+#https://stackoverflow.com/questions/1726402/in-python-how-do-i-use-urllib-to-see-if-a-website-is-404-or-200
