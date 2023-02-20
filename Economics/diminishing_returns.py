8092a880d245ae6e3296380e4c41513c263b7933
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-02-21 00:49:06 +0100

    commenting

--- Economics/diminishing_returns.py
+++ Economics/diminishing_returns.py
@@ -1,20 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Set up the x-axis with range from 0 to 10
+# All codes are in English 
+# First of all we set up the x-axis with range from 0 to 10
 x = np.arange(0, 10, 0.1)
 
-# Define a function to calculate the y-axis values based on the x-axis values
+# We define a function to calculate the y-axis values based on the x-axis values
 def diminishing_returns(x):
     return 10 / (1 + np.exp(-x))
 
-# Calculate the y-axis values for the function using the x-axis values
+# Then we calculate the y-axis values for the function using the x-axis values
 y = diminishing_returns(x)
 
-# Create a plot with x-axis as input and y-axis as output
+# plot it
 plt.plot(x, y, label="Diminishing Returns")
 
-# Add title, labels and legend to the plot
+#  Tittle blocks
 plt.title("Diminishing Returns")
 plt.xlabel("Input")
 plt.ylabel("Output")
