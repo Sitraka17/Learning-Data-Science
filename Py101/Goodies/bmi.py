640afddd2961b75bb8c10dcb39f6dc6a3a8b3520
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-03-18 22:10:36 +0100

    #commented

--- Py101/Goodies/bmi.py
+++ Py101/Goodies/bmi.py
@@ -3,20 +3,15 @@
 
 
 # height_in and weight_lb are available as a regular lists
-
-# Import numpy
-import numpy as np
-
 # Calculate the BMI: bmi
-np_height_m = np.array(height_in) * 0.0254
-np_weight_kg = np.array(weight_lb) * 0.453592
-bmi = np_weight_kg / np_height_m ** 2
-
+#np_height_m = np.array(height_in) * 0.0254
+#np_weight_kg = np.array(weight_lb) * 0.453592
+#bmi = np_weight_kg / np_height_m ** 2
 # Create the light array
-light = bmi < 21 
-np_light = np.array(light)
+#light = bmi < 21 
+#np_light = np.array(light)
 # Print out light
-print(light)
+#print(light)
 
 # Print out BMIs of all baseball players whose BMI is below 21
 #print(bmi[light])
