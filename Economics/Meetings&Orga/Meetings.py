3bc5aad4e5537f1a369f6c2c82bf722348a2ade5
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-06-10 08:17:47 +0200

    Update Meetings.py

--- Economics/Meetings&Orga/Meetings.py
+++ Economics/Meetings&Orga/Meetings.py
@@ -1,2 +1,34 @@
 #Lists of econ meetings
 #Goal is to scrap all the econ conf in France, then Europe then WW
+
+
+Regulating housing quality:
+evidence from France*
+Antoine Levy†
+February 2023
+
+Preliminary draft
+
+Abstract
+
+Minimum quality standards in housing markets can affect the equilibrium quantity of dwellings supplied,
+
+depending on the demand response to their shadow monetary and non-monetary costs. To estimate the conse-
+quences of quality regulations, this paper studies a rule requiring new single-family constructions in France to
+
+use the services of an architect, whenever their square footage exceeds a threshold. Using exhaustive admin-
+istrative data on the universe of building permits in France and quasi-experimental variation in the threshold,
+
+I evidence that the cost of new housing production jumps above the notch, which in turn leads to substantial
+
+distortions in the size distribution of homes. New units bunch below the regulatory notch to avoid addi-
+tional costs, and additions to existing dwellings are downsized or avoided to circumvent the requirement.
+
+Regression discontinuity estimates reveal that homeowners characteristics, dwelling type, and spatial loca-
+tion choices all change sharply at the threshold, evidencing a segmentation of households by their taste for
+
+housing quality. Overall, the attribute-based regulation leads to significant misallocation of housing consump-
+tion.
+
+JEL codes: R52, R21, R38.
+Keywords: housing demand, bunching, building codes, quality standards.
