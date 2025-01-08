0255cbdd9781f3a3917f51c039380356df754543
SitrakaReveals <146767706+SitrakaReveals@users.noreply.github.com>
2025-01-08 09:07:12 +0100

    cleanning

--- Goodies/Map.py
+++
@@ -1,7 +0,0 @@
-#better to use conda for this.
-conda config --prepend channels conda-forge
-conda create -n ox --strict-channel-priority osmnx
-import osmnx as ox
-
-place = ["Berlin, Germany"]
-G = ox.graph_from_place(place, retain_all=True, simplify = True, network_type='all')
